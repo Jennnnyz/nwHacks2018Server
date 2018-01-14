561be727e5fdd24b54ed66ebc2ef2d4aa3685168
Jenny Zhang <jennyz1105@gmail.com>
2018-01-13 18:32:26 -0800

    added get and post

--- index.js
+++ index.js
@@ -4,11 +4,116 @@ var app = express();
 app.set('port', (process.env.PORT || 1337));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/products', function(request, response) {
+app.get('/games', function(request, response) {
 
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    response.json("heehee");
+  response.json("games");
+})
+
+app.post('/games', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json("games");
+})
+
+app.get('/games/:gameId', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json(request.params.gameId);
+})
+
+app.post('/games/:gameId', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json(request.params.gameId);
+})
+
+app.get('/games/:gameId/users', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json("users");
+})
+
+app.post('/games/:gameId/users', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json("users");
+})
+
+app.get('/games/:gameId/users/:userId', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json(request.params.userId);
+})
+
+app.post('/games/:gameId/users/:userId', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json(request.params.userId);
+})
+
+app.get('/games/:gameId/users/:userId/items', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json("items");
+})
+
+app.post('/games/:gameId/users/:userId/items', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json("items");
+})
+
+app.get('/games/:gameId/users/:userId/items/:itemId', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json(request.params.itemId);
+})
+
+app.post('/games/:gameId/users/:userId/items/:itemId', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json(request.params.itemId);
+})
+
+app.get('/games/:gameId/items', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json("items");
+})
+
+app.post('/games/:gameId/items', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json("items");
+})
+
+app.get('/games/:gameId/items/:itemId', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json(request.params.itemId);
+})
+
+app.post('/games/:gameId/items/:itemId', function(request, response) {
+
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.json(request.params.itemId);
 })
 
 app.listen(app.get('port'), function() {

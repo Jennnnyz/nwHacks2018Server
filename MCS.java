ff4d7fb0d968b4010cee05df583a51d17870967a
Jenny Zhang <jennyz1105@gmail.com>
2018-01-13 22:42:29 -0800

    MCS

--- MCS.java
+++ MCS.java
@@ -1,3 +1,4 @@
+    //To process the image
     private String process() throws VisionServiceException, IOException {
         Gson gson = new Gson();
         String[] features = {"ImageType", "Color", "Faces", "Adult", "Categories"};
@@ -15,7 +16,11 @@
         return result;
     }
 
+//To create the client
     if (client==null){
     client = new VisionServiceRestClient(getString(R.string.subscription_key), getString(R.string.subscription_apiroot));
 }
 
+//To get the category
+AnalysisResult v = process();
+Category[] = v.Categories;
